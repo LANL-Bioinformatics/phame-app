c49d6814d90307d053446e5bae699c4ee1b19a75
mflynn-lanl <mflynn@lanl.gov>
2018-06-27 08:29:47 -0600

    adding human readable choices

--- phame_api01/phame_app/models.py
+++ phame_api01/phame_app/models.py
@@ -5,17 +5,30 @@ class Run(models.Model):
     ref_dir = models.FileField(null=True, upload_to='refdir')
     work_dir = models.FileField(null=True, upload_to='workdir')
 
-    REF_CHOICES = (('0', '0'), ('1', '1'), ('2', '2'))
+    RANDOM = '0'
+    GIVEN = '1'
+    ANI = '2'
+    REF_CHOICES = ((RANDOM, 'random'), (GIVEN, 'given'), (ANI, 'ANI'))
     reference = models.CharField(max_length=6, choices=REF_CHOICES)
 
     ref_file = models.FileField(null=True)
     project = models.CharField(max_length=20, default='ecoli')
-    CDS_CHOICES = (('0', '0'), ('1', '1'))
+
+    NO_CDS = '0'
+    CDS_SNPS = '1'
+    CDS_CHOICES = ((NO_CDS, 'no cds SNPs'), (CDS_SNPS, 'cds SNPs'))
     cds_snps = models.CharField(max_length=1, default=False)
 
     BOOL_CHOICES = (('0', '0'), ('1', '1'))
-    buildSNPdb = models.CharField(max_length=1, choices=BOOL_CHOICES)
-    FIRST_CHOICES = (('1', '1'), ('2', '2'))
+
+    ALIGN = '0'
+    BUILD = '1'
+    BUILD_CHOICES = ((ALIGN, 'only align to reference'), (BUILD, 'build SNP database'))
+    buildSNPdb = models.CharField(max_length=1, choices=BUILD_CHOICES)
+
+    FIRST = '1'
+    UPDATE_ALIGNMENT = '2'
+    FIRST_CHOICES = ((FIRST, 'yes'), (UPDATE_ALIGNMENT, 'update existing SNP alignment'))
     first_time = models.CharField(max_length=3, choices=FIRST_CHOICES)
 
     DATA_CHOICES = (('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'),
