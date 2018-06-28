79567b6863e7a97a50f127309db91517a9aacbd1
mflynn-lanl <mflynn@lanl.gov>
2018-06-28 16:46:59 -0600

    added css and javascript to template

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,6 +67,7 @@ THIRD_PARTY_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework',
+    'widget_tweaks',
 ]
 LOCAL_APPS = [
     'phame_api01.users.apps.UsersAppConfig',
