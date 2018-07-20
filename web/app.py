71e4cd9bd66d25f4a89728c65cf1ca7194128b5d
mflynn-lanl <mflynn@lanl.gov>
2018-07-20 14:33:22 -0600

    creates separate directories for each project, works from commandline

--- web/app.py
+++ web/app.py
@@ -7,36 +7,51 @@ from werkzeug.utils import secure_filename
 import subprocess
 from forms import LoginForm, InputForm
 from config import Config
+import logging
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
+logging.basicConfig(filename='phame.log', level=logging.DEBUG)
+logging.debug(app.config['PROJECT_DIRECTORY'])
+
 def upload_files(request, form):
+    project_dir = os.path.join(app.config['PROJECT_DIRECTORY'], form.project.data)
+    ref_dir = os.path.join(project_dir, 'refdir')
+    work_dir = os.path.join(project_dir, 'workdir')
+    logging.debug(project_dir)
+    os.makedirs(project_dir, exist_ok=True)
     if form.reference_file.data:
         reference_file = request.files['reference_file']
         filename = secure_filename(reference_file.filename)
-        reference_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        reference_file.save(os.path.join(project_dir, filename))
 
     if form.ref_dir.data:
+        os.makedirs(ref_dir, exist_ok=True)
         for file_name in request.files.getlist("ref_dir"):
             filename = secure_filename(file_name.filename)
-            file_name.save(os.path.join(app.config['UPLOAD_FOLDER'], 'refdir',  filename))
+            file_name.save(os.path.join(ref_dir, filename))
 
     if form.work_dir.data:
+        os.makedirs(work_dir, exist_ok=True)
         for file_name in request.files.getlist("work_dir"):
             filename = secure_filename(file_name.filename)
-            file_name.save(os.path.join(app.config['UPLOAD_FOLDER'], 'workdir',  filename))
+            file_name.save(os.path.join(work_dir, filename))
 
 
 @app.route('/run/<project>')
 def run_phame(project):
     try:
-        p1 = subprocess.Popen('./docker_run_phame.sh', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p1 = subprocess.Popen('./docker_run_phame.sh {0}'.format(project), shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = p1.communicate()
+        logging.debug(stdout)
+        logging.error(stderr)
 
     except subprocess.CalledProcessError as e:
+        logging.error(str(e))
         return "An error occurred while trying to run PhaME: {0}".format(str(e))
 
-    return redirect(url_for('display', tree_file ='{0}_all.fasttree'.format(project)))
+    return redirect(url_for('display', project = project))
 
 @app.route('/')
 @app.route('/index')
@@ -45,10 +60,11 @@ def index():
     return render_template('index.html', title='Home', user=user)
 
 
-@app.route('/display/<tree_file>', methods=['POST', 'GET'])
-def display(tree_file):
-    source = os.path.join('/phame_api01/phame_api01/media/workdir/results/', tree_file)
-    target = os.path.join('static', tree_file)
+@app.route('/display/<project>', methods=['POST', 'GET'])
+def display(project):
+    tree_file = '{0}_all.fasttree'.format(project)
+    source = os.path.join(app.config['PROJECT_DIRECTORY'], project, 'workdir', 'results', tree_file)
+    target = os.path.join(os.path.dirname(__file__), 'static', tree_file)
     if not os.path.exists(target):
         os.symlink(source, target)
     return render_template('tree_output.html', tree= tree_file)
@@ -65,14 +81,15 @@ def input():
 
     if form.validate_on_submit():
         form_dict = request.form.to_dict()
+        project = form_dict['project']
         form_dict.pop('csrf_token')
-        form_dict['ref_dir'] = '../media/refdir'
-        form_dict['work_dir'] = '../media/workdir'
+        form_dict['ref_dir'] = '../{0}/refdir/'.format(project)
+        form_dict['work_dir'] = '../{0}/workdir'.format(project)
         form_dict['reference_file'] = form.reference_file.data.filename
         content = render_template('phame.tmpl', form=form_dict)
-        with open('/phame_api01/phame_api01/media/config.ctl', 'w') as conf:
+        with open(os.path.join(app.config['PROJECT_DIRECTORY'], project, 'config.ctl'), 'w') as conf:
             conf.write(content)
-        return redirect(url_for('run_phame', project=form_dict['project']))
+        return redirect(url_for('run_phame', project=project))
     return render_template('input.html', title='Phame input', form=form)
 
 
--- web/docker_run_phame.sh
+++ web/docker_run_phame.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-source activate phame
-/PhaME-1/src/runPhaME.pl /phame_api01/phame_api01/media/config.ctl
+/PhaME-1/src/runPhaME.pl /phame_api01/phame_api01/media/$1/config.ctl
