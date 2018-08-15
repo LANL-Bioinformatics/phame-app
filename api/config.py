98f3f876cacb199913f97ad7c081bddc4eef4ad4
mflynn-lanl <mflynn@lanl.gov>
2018-08-15 13:40:38 -0600

    using celery and redis to dispatch calls to phame

---
+++ api/config.py
@@ -0,0 +1,10 @@
+import os
+
+basedir = os.path.dirname(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    PROJECT_DIRECTORY = os.path.join(basedir, 'phame_api01', 'media')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgres:///phame_api01'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
