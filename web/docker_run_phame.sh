3c02d9bcbc59213f12ab2866e44232c1be660da1
mflynn-lanl <mflynn@lanl.gov>
2018-09-24 07:41:37 -0600

    removed call to shell script

--- web/docker_run_phame.sh
+++ web/docker_run_phame.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo $1
-/PhaME-1/src/runPhaME.pl /phame_api/media/$1/config.ctl
-#/usr/local/bin/runPhaME /phame_api/media/$1/config.ctl
+#/PhaME-1/src/runPhaME.pl /phame_api/media/$1/config.ctl
+/usr/local/bin/runPhaME /phame_api/media/$1/config.ctl
--- web/tasks.py
+++ web/tasks.py
@@ -2,6 +2,7 @@ import os
 import time
 from celery import Celery
 import subprocess
+from subprocess import PIPE
 from celery.utils.log import get_task_logger
 from json import dumps
 
@@ -12,20 +13,30 @@ celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 
 logger = get_task_logger(__name__)
 
+
+def log_subprocess_output(pipe):
+    for line in iter(pipe.readline, b''): # b'\n'-separated lines
+        logger.info('got line from subprocess: %r', line)
+
+
 @celery.task(name='tasks.run_phame', bind=True)
 def phame_run(self, project, username):
     try:
-        logger.info('run_phame called with {0}/{1}'.format(project, username))
+        logger.info("run_phame called '/usr/local/bin/runPhaME /phame_api/media/{0}/{1}/config.ctl".format(project, username))
         cmd = ['./docker_run_phame.sh', '{0}/{1}'.format(project, username) ]
-        p1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # p1 = subprocess.Popen('./docker_run_phame.sh {0}/{1}'.format(project, username), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        p1 = subprocess.Popen(['/usr/local/bin/runPhaME /phame_api/media/{0}/{1}/config.ctl'.format(project, username)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         logger.info('process launched')
-        stdout, stderr = p1.communicate()
-        logger.debug(stdout)
-        logger.debug(stderr)
+        with p1.stdout:
+            log_subprocess_output(p1.stdout)
+        # exitcode = p1.wait()
+        # stdout, stderr = p1.communicate()
+        # logger.debug(stdout)
+        # logger.debug(stderr)
     except subprocess.CalledProcessError as e:
         logger.error(str(e))
         return dumps({'error': str(e)})
     except celery.exceptions.TaskError as e:
         logger.error(str(e))
 
-    return stdout
+    return p1.stdout
