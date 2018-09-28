8b20dc1076291dd33ad4ec9d750b5f4e591e4e94
mflynn-lanl <mflynn@lanl.gov>
2018-09-28 12:39:26 -0600

    removed return statement in tasks that was causing error

--- web/tasks.py
+++ web/tasks.py
@@ -15,28 +15,30 @@ logger = get_task_logger(__name__)
 
 
 def log_subprocess_output(pipe):
-    for line in iter(pipe.readline, b''): # b'\n'-separated lines
-        logger.info('got line from subprocess: %r', line)
+    try:
+        for line in iter(pipe.readline, b''): # b'\n'-separated lines
+            logger.info('subprocess output: %r', line)
+        logger.info('Done writing output')
+    except Exception as e:
+        logger.error('Error writing output {0}'.format(str(e)))
 
 
 @celery.task(name='tasks.run_phame', bind=True)
 def phame_run(self, project, username):
     try:
         logger.info("run_phame called '/usr/local/bin/runPhaME /phame_api/media/{0}/{1}/config.ctl".format(project, username))
-        cmd = ['./docker_run_phame.sh', '{0}/{1}'.format(project, username) ]
-        # p1 = subprocess.Popen('./docker_run_phame.sh {0}/{1}'.format(project, username), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        p1 = subprocess.Popen(['/usr/local/bin/runPhaME /phame_api/media/{0}/{1}/config.ctl'.format(project, username)], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        cmd = ['runPhaME', '/phame_api/media/{0}/{1}/config.ctl'.format(project, username)]
+        p1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         logger.info('process launched')
+
         with p1.stdout:
+            # self.update_state(state='RUNNING', meta=p1.stdout)
             log_subprocess_output(p1.stdout)
-        # exitcode = p1.wait()
-        # stdout, stderr = p1.communicate()
-        # logger.debug(stdout)
-        # logger.debug(stderr)
+        logger.info('Exited writing output')
+
     except subprocess.CalledProcessError as e:
         logger.error(str(e))
         return dumps({'error': str(e)})
     except celery.exceptions.TaskError as e:
         logger.error(str(e))
 
-    return p1.stdout
