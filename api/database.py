8c0bc8f4007d1810dc5dc05e3774ba5fe7f8d223
mflynn-lanl <mflynn@lanl.gov>
2018-11-23 07:46:14 -0700

    increased size limit for downloads, increased size of db connection pool

--- api/database.py
+++ api/database.py
@@ -9,7 +9,8 @@ pwd = os.environ['POSTGRES_PASSWORD']
 db = os.environ['POSTGRES_DB']
 host = os.environ['POSTGRES_HOST']
 port = os.environ['POSTGRES_PORT']
-engine = create_engine('postgres://%s:%s@%s:%s/%s' % (user, pwd, host, port, db))
+pool_size = 10
+engine = create_engine('postgres://%s:%s@%s:%s/%s' % (user, pwd, host, port, db), pool_size=pool_size)
 
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
