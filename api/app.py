f4ab544d88a1d8d46f65a4158347cb9111b7ac59
mflynn-lanl <mflynn@lanl.gov>
2018-11-28 08:40:58 -0700

    debug=False

--- api/app.py
+++ api/app.py
@@ -782,5 +782,5 @@ def display(project, log_time=None):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=5090)
+    app.run(debug=False,host='0.0.0.0', port=5090)
     # app.run(debug=True)
