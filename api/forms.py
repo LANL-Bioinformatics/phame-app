690ba1c83c1cf6b56270358027cb56f3215f5120
mflynn-lanl <mflynn@lanl.gov>
2018-12-12 13:42:46 -0700

    public login added to form

--- api/forms.py
+++ api/forms.py
@@ -11,6 +11,7 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
+    public_login = BooleanField('Skip login to view public projects')
     submit = SubmitField('Sign In')
 
 
