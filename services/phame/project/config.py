c096b1aff6ec62a3c98c745f4746fcfc242323b4
mflynn-lanl <mflynn@lanl.gov>
2019-08-29 16:30:43 -0600

    made secret-key an environment variable

--- services/phame/project/config.py
+++ services/phame/project/config.py
@@ -8,7 +8,7 @@ class BaseConfig:
     """Base configuration"""
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     MAX_CONTENT_LENGTH = 1000 * 1024 * 1024
